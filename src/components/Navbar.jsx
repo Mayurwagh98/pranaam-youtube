95e6af28bce91a35a4f40301a3ae276a55c7350e
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-11-07 22:01:33 +0530

    added: navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoLogoYoutube } from "react-icons/io";
+import { GiPlagueDoctorProfile } from "react-icons/gi";
+import { IoIosSearch } from "react-icons/io";
+
+const Navbar = () => {
+  return (
+    <nav className="grid grid-flow-col p-2 h-[4.5rem] shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)]">
+      <div className="flex col-span-1 justify-start items-center">
+        <GiHamburgerMenu className="mx-2 text-xl cursor-pointer" />
+        <h1 className="flex justify-center items-center text-xl font-bold cursor-pointer">
+          <IoLogoYoutube className="text-red-600 text-2xl mr-1 ml-2" />
+          Youtube
+        </h1>
+      </div>
+
+      <div className="flex col-span-10 w-1/2 justify-center items-center mx-auto">
+        <input
+          type="search"
+          placeholder="Search here"
+          className="border border-gray-400 outline-none w-full p-2 rounded-l-full"
+        />
+        <button className="p-2 border border-gray-400 rounded-r-full bg-gray-100">
+          <IoIosSearch className="text-2xl w-[50px]" />
+        </button>
+      </div>
+
+      <div className="col-span-1 grid place-items-center">
+        <GiPlagueDoctorProfile className="text-4xl cursor-pointer border border-gray-400 p-2 rounded-full" />
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
