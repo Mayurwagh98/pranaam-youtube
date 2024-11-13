8a1ef89964d70d8b74f627fe3fe3ab207520c65e
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-11-14 00:50:25 +0530

    Fetching: data from live api

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,4 +1,20 @@
+import { useEffect } from "react";
+import { YOUTUBE_VIDEOS_API } from "../utils/constants";
+
 const VideoContainer = () => {
+  useEffect(() => {
+    getAllVideos();
+  }, []);
+
+  const getAllVideos = async () => {
+    try {
+      const data = await fetch(YOUTUBE_VIDEOS_API);
+      const jsonData = await data.json();
+      console.log(jsonData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return <div className="flex flex-wrap"></div>;
 };
 
