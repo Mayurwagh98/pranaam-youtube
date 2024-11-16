125669dccb5879fe020f23a471a4c8ddf79dfdf2
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-11-16 21:08:05 +0530

    OnFouc and onBlur

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,6 +12,7 @@ const Navbar = () => {
   const dispatch = useDispatch();
   const [searchQuery, setSearchQuery] = useState("");
   const [suggesstions, setSuggesstions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
   const toggleSidebar = () => {
     dispatch(showSidebar());
@@ -52,16 +53,21 @@ const Navbar = () => {
             placeholder="Search here"
             className="border border-gray-400 outline-none w-full p-2 rounded-l-full"
             onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
           />
           <button className="p-2 border border-gray-400 rounded-r-full bg-gray-100">
             <IoIosSearch className="text-2xl w-[50px]" />
           </button>
         </div>
 
-        {suggesstions?.length > 0 && (
+        {showSuggestions && (
           <div className="absolute bg-white py-1 px-2 w-[37%] shadow-lg rounded-lg border border-gray-100 mt-[550px]">
             {suggesstions?.map((item, index) => (
-              <p key={index} className="my-2 flex justify-start items-center hover:bg-gray-200 p-2">
+              <p
+                key={index}
+                className="my-2 flex justify-start items-center hover:bg-gray-200 p-2"
+              >
                 <CiSearch className="mr-1" />
                 {item}
               </p>
