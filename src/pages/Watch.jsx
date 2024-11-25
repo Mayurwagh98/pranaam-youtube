f71cc6beb57b15af7340958c00bd07b3264b4020
mayur wagh <mayurwagh@mayurs-Air.bwrouter>
2024-11-25 20:51:33 +0530

    Adding: chat section

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -11,15 +11,21 @@ const Watch = () => {
     dispatch(closeSider());
   }, []);
   return (
-    <div className="p-4">
-      <iframe
-        width="1000"
-        height="500"
-        src={`https://www.youtube.com/embed/${searchParams.get("v")}`}
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        className="rounded-lg"
-      ></iframe>
+    <div className="p-4 w-full">
+      <div className="flex">
+        <div>
+          <iframe
+            width="1000"
+            height="500"
+            src={`https://www.youtube.com/embed/${searchParams.get("v")}`}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            className="rounded-lg"
+          ></iframe>
+        </div>
+        <div className="w-full border h-[500px] ml-3 rounded-lg overflow-y-auto bg-gray-100"></div>
+      </div>
+
       <CommentsContainer />
     </div>
   );
